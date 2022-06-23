682da2a4455586b3ae1c18c1d9064997a246f77f
Raxxius <raxxman@gmail.com>
2022-06-23 11:55:55 +0100

    started card component organisation

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+    return (
+        <div>
+            <h1>Img</h1>
+            <h1>Location</h1>
+            <h1>Name</h1>
+            <h1>Dates</h1>
+            <h1>Description</h1>
+        </div>
+    )
+}
+
+export default Card;
