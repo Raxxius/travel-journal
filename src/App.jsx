51cff9ceeaaa3471199f1973e787f786b62f5358
Raxxius <raxxman@gmail.com>
2022-06-23 11:56:06 +0100

    started card component organisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import Globe from './resources/images/globe.jsx'
+import Card from './components/Card'
 
 function App() {
     return (
         <>
             <div className='header'>
-                
                 <Globe />
                 <h1 className='title'>my travel journal.</h1>
             </div>
+            <Card />
         </>
     )
 }
